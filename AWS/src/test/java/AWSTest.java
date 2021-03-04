78e783273ef6798c86d283b8023db8c37ac3b3b4
Keerthisaraa <keerthisaraaa@gmail.com>
2021-03-03 22:33:24 -0600

    Fixing the deliberate error

--- AWS/src/test/java/AWSTest.java
+++ AWS/src/test/java/AWSTest.java
@@ -169,7 +169,7 @@ class AWSTest {
 	@Test
 	void testStepMultiplier() {
 		int[] array = {0, 1, 11, 21, 91, 101};
-		int[] expected = {0, 2, 44, 2100, 9100, 10100};
+		int[] expected = {0, 2, 44, 2100, 9100, 101};
 		AWS aws = new AWS(array);
 		int[] result = aws.stepMultiplier();
 		for (int i = 0; i <  result.length; i++) {
